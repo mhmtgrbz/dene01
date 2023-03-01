812bc266cfc6f8a47665c19eb68d0c69351c7957
Mehmet Gurbuz <email_adresiniz>
2023-03-01 10:24:43 +0300

    throw exceptionsss

--- src/main/java/s.java
+++ src/main/java/s.java
@@ -9,6 +9,7 @@ public class s {
         System.out.println("Merhaba");
         System.out.println("Merhaba");
         System.out.println("yeni");
+        System.out.println("throw exception");
 
     }
 
