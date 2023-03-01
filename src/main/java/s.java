bdb2bdb2fd599f7660a5f8d87525a2b47bee6c50
Mehmet Gurbuz <email_adresiniz>
2023-03-01 10:03:29 +0300

    v3

--- src/main/java/s.java
+++ src/main/java/s.java
@@ -2,7 +2,12 @@ public class s {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
 
     }
 
