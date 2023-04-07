139c2663a5e2646ad32db907664bcd427713cf18
Mehmet Gurbuz <mhmt999999@gmail.com>
2023-04-08 01:46:44 +0300

    Versiyonagustos

--- src/main/java/s.java
+++ src/main/java/s.java
@@ -2,7 +2,22 @@ public class s {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("yeni");
+        System.out.println("throw exception");
+        System.out.println("hakan");
+        System.out.println("hakan2");
+        System.out.println("hakan3");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
 
     }
 
