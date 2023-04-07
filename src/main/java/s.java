0cd6940a073f0bd221132544f0eba82213800033
Mehmet Gurbuz <email_adresiniz>
2023-04-08 00:49:12 +0300

    haziransonu

--- src/main/java/s.java
+++ src/main/java/s.java
@@ -2,8 +2,22 @@ public class s {
 
     public static void main(String[] args) {
 
-        //Nisan ayı çalışmam
-
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("Merhaba");
+        System.out.println("yeni");
+        System.out.println("throw exception");
+        System.out.println("hakan");
+        System.out.println("hakan2");
+        System.out.println("hakan3");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
+        System.out.println("mayisayi");
 
     }
 
